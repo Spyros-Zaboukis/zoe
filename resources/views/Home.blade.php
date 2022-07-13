3d0827c6d0c650289c32e76a320c5e736b3e2130
Spyros-Zaboukis <Spyros.Zaboukis.83@gmail.com>
2022-07-13 11:28:01 +0200

    send repos with config laravel and first view

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.templateHtml')
+
+@section('headTitle')
+Welcome
+@endsection
+
+
+
+@section('content')
+
+
+<h1>The truth is what Google says about you!</h1>
+    <p>
+        Welcome to your site, this is your space, a space that looks like you and that really reflects what you want to highlight about your personality.
+        It complements your CV and cover letter.
+        This is your reference!
+    </p>
+
+    <h2>Create your profile and share it!</h2>
+    <button>S'inscrire</button>
+
+
+    <h2>Learn more</h2>
+    <p>
+        be among the first to create your profile and share it each time you are asked for your site.
+        Do not hesitate to promote yourself to your future boss,
+        a business or human resources department.
+        This is where you can express yourself.
+    </p>
+    <button>Enter in the site</button>
+
+@endsection
+
+
